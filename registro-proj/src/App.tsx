9da2e606e8eef4b2397994322caabf8bf42a0e61
AndreaBenevelli <andrea.benevelli123@gmail.com>
2025-09-14 16:56:36 +0200

    app import

--- registro-proj/src/App.tsx
+++ registro-proj/src/App.tsx
@@ -5,7 +5,6 @@ import type { User } from "./models/User";
 
 import LandingPage from "./components/LandingPage/LandingPage";
 import LoginPage from "./components/LoginPage/LoginPage";
-import SignUpPage2 from "./components/SignupPage/SignUpPage2";
 import SignupPage from "./components/SignupPage/SignupPage";
 import HomePage from "./components/HomePage/HomePage";
 import Dashboard from "./components/Dashboard/Dashboard";
@@ -60,7 +59,7 @@ export default function App() {
       <Route
         path="/signup"
         element={
-          <SignUpPage2
+          <SignupPage
             onSignup={(userIn: User) => {
               setIsLogged(true);
               setLoggedUser(userIn);
